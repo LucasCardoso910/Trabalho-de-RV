1fdeba2e3022b6347d437539fd6a0142d02eed87
lucascardoso910 <lucascardoso910@gmail.com>
2021-11-01 02:52:01 -0300

    assets: scripts: created feather fall script
    Create a script to control the feather fall. Currently, there is no way
    to simulate the movements that a feather does when it's falling with air
    resistance. Since the idea of the project is to compare the movement
    both with and without air resistance, this is much needed.
    
    It actually doesn't correctly simulates physically the forces envolved
    in the movement, but actually with two mathematical equations: a sin
    equation to simulate the movement in one way and another in the x axis
    and a simple constant velocity equation to calculate the coordinate in
    the y axis.

---
+++ Laboratório de Física/Assets/Scripts/FeatherFall.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float velocity = 5;
+    public float factor = 5;
+    public GameObject feather;
+    public GameObject floor;
+
+    private Rigidbody teste;
+    private DateTime startTime;
+    private Vector3 initialPosition;
+    private float floorHeight;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startTime = DateTime.Now;
+        initialPosition = feather.transform.position;
+
+        floorHeight = floor.transform.position.y + (floor.transform.localScale.y / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double time = getTime();
+
+        Vector3 currentPosition = feather.transform.position;
+        float x = initialPosition.x + (float) (factor * Math.Sin((time) / 500));
+        float y = initialPosition.y - calculateHeight((float) time);
+        float z = initialPosition.z;
+
+        if (y <= floorHeight) {
+            y = floorHeight;
+            x = currentPosition.x;
+            z = currentPosition.z;
+        }
+
+        feather.transform.position = new Vector3(x, y, z);
+    }
+
+    float calculateHeight(float t) {
+        return (t * velocity) / 1000.0f;
+    } 
+
+    double getTime() {
+        return ((DateTime.Now - startTime).TotalMilliseconds);
+    }
+}
