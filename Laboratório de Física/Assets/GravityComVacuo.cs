4152b4472f9bbded609cf472130fc00909a829d0
Victor Kikuti <victor.kikuti@usp.br>
2021-11-06 20:18:26 -0300

    atualizacao 06/11/2021 experimento 1 - pena martelo
    Signed-off-by: Victor Kikuti <victor.kikuti@usp.br>

---
+++ Laboratório de Física/Assets/GravityComVacuo.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityComVacuo : MonoBehaviour
+{
+    public GameObject dude;
+    private Rigidbody rb;
+    public GameObject dude1;
+    private Rigidbody rb1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = dude.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb1 = dude1.GetComponent<Rigidbody>();
+        rb1.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetPosition()
+    {
+        dude.transform.position = new Vector3(-9, 9, 14);
+        dude1.transform.position = new Vector3(-14, 9, 14);
+        dude.transform.eulerAngles = new Vector3(0, -90, 90);
+        dude1.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+    }
+
+    public void EnableGravity()
+    {
+        rb.useGravity = true;
+        rb1.useGravity = true;
+
+    }
+
+    public void DisableGravity()
+    {
+        rb.useGravity = false;
+        rb1.useGravity = false;
+
+    }
+
+    public void ResetPosition()
+    {
+        DisableGravity();
+        SetPosition();
+    }
+}
