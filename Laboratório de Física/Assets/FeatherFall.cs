e7354cf8cb0aa5bbb7c59efd9f26f4434c802781
Victor Kikuti <victor.kikuti@usp.br>
2021-11-08 20:56:45 -0300

    Mudança do experimento pena-martelo. Anterior = duas cameras, uma com vacuo, e outra sem. Posterior = unica camera, adicionando um botao que ativa e desativa o vacuo OBS: Resolvendo problema da queda especifica da pena

---
+++ Laboratório de Física/Assets/FeatherFall.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatherFall : MonoBehaviour
+{ 
+    public float velocity = 5;
+    public float factor = 5;
+    public GameObject feather;
+    public GameObject floor;
+
+    private Rigidbody teste;
+    private DateTime startTime;
+    private Vector3 initialPosition;
+    private float floorHeight;
+    private Rigidbody rb;
+    private bool desactivated = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startTime = DateTime.Now;
+        initialPosition = feather.transform.position;
+
+        floorHeight = floor.transform.position.y + (floor.transform.localScale.y / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double time = 0;
+        if (!desactivated)
+        {
+            time = getTime();
+        }
+
+        Vector3 currentPosition = feather.transform.position;
+        float x = initialPosition.x + (float)(factor * Math.Sin((time) / 500));
+        float y = initialPosition.y - calculateHeight((float)time);
+        float z = initialPosition.z;
+
+        if (y <= floorHeight)
+        {
+            y = floorHeight;
+            x = currentPosition.x;
+            z = currentPosition.z;
+        }
+
+        feather.transform.position = new Vector3(x, y, z);
+    }
+
+    float calculateHeight(float t)
+    {
+        return (t * velocity) / 1000.0f;
+    }
+
+    double getTime()
+    {
+        return ((DateTime.Now - startTime).TotalMilliseconds);
+    }
+
+    public void reactivate()
+    {
+        desactivated = false;
+        velocity = 5;
+        factor = 3;
+        startTime = DateTime.Now;
+    }
+
+    public void desactivate()
+    {
+        desactivated = true;
+        velocity = 0;
+        factor = 0;
+        feather.transform.position = new Vector3(30, 18, 14);
+    }
+
+}
+
---
+++ Laboratório de Física/Assets/Gravity.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    public GameObject dude;
+    private Rigidbody rb;
+    public GameObject dude1;
+    private Rigidbody rb1;
+    public bool ehVacuo = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = dude.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb1 = dude1.GetComponent<Rigidbody>();
+        rb1.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetPosition()
+    {
+        dude.transform.position = new Vector3(40, 18, 14);
+        dude1.transform.position = new Vector3(30, 18, 14);
+        dude.transform.eulerAngles = new Vector3(0, -90, 90);
+        dude1.transform.eulerAngles = new Vector3(-90, 0, 90);
+
+    }
+
+    public void EnableGravity()
+    {
+        rb.useGravity = true;
+        rb1.useGravity = true;
+
+    }
+
+    public void DisableGravity()
+    {
+        rb.useGravity = false;
+        rb1.useGravity = false;
+
+    }
+
+    public void ResetPosition()
+    {
+        DisableGravity();
+        SetPosition();
+    }
+
+    public void changeDrag()
+    {
+        if (ehVacuo == false)
+        {
+            ehVacuo = true;
+        }
+        else if (ehVacuo == true)
+        {
+            ehVacuo = false;
+        }
+
+        if (ehVacuo == false)
+        {
+            rb.drag = 0.2F;
+            rb1.drag = 2;
+        }
+        else {
+            rb.drag = 0;
+            rb1.drag = 0;
+        }
+    }
+}
