424c7de1e7970fbc312501ef74a87636f8983a30
Victor Kikuti <victor.kikuti@usp.br>
2021-11-15 19:50:25 -0300

    Adiocionamos animação de abrir e fechar a tampa a partir do hover button
    Signed-off-by: Victor Kikuti <victor.kikuti@usp.br>

---
+++ Laboratório de Física/Assets/ControleTampar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleTampar : MonoBehaviour
+{
+    public Animator anim;
+    private bool controler;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controler)
+        {
+            anim.SetBool("controleTampa", false);
+        }
+        else
+        {
+            anim.SetBool("controleTampa", true);
+        }
+    }
+
+    public void SwitchControler()
+    {
+        if (controler)
+        {
+            controler = false;
+        }
+        else
+        {
+            controler = true;
+        }
+    }
+}
